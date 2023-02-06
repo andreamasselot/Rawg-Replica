a653f0eaee654c4f8519ebfaa20752f19b69b8c8
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-02-06 12:01:52 +0100

    Homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import "../styles/Header.css";
+import logo from "../assets/img/logo.png";
 
 const Header = () => {
   return (
     <div className="container">
       <header>
         <div className="logo-title">
+          <img src={logo} alt="logo gamepad" className="logo" />
           <p>Gamepad</p>
         </div>
       </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import "../styles/Home.css";
+import logo from "../assets/img/logo.png";
 import Header from "../components/Header";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div className="container">HOME</div>
+      <div className="container">
+        <section className="searchBar">
+          <div className="title-searchBar logo-title">
+            <img src={logo} alt="gamepad logo" />
+            <h1>Gamepad</h1>
+          </div>
+          <input type="search" />
+        </section>
+      </div>
     </>
   );
 };
